5b37099b44cab00637ca4226a149d32f3f810a4d
Nandini Asokan <nandini.asokan92@gmail.com>
2023-09-06 21:02:51 -0400

    Updated order service and README

--- src/main/java/com/gogo_ecommerce/gogo/order/OrderController.java
+++ src/main/java/com/gogo_ecommerce/gogo/order/OrderController.java
@@ -3,36 +3,43 @@ package com.gogo_ecommerce.gogo.order;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.gogo_ecommerce.gogo.cart.Cart;
-import com.gogo_ecommerce.gogo.item.Item;
 
 @RestController
 public class OrderController {
     @Autowired
     OrderService orderService;
 
+    @GetMapping("/order/{id}")
+    public ResponseEntity<OptionalOrderResponse> getOrder(@PathVariable String id){
+        Optional<Order> order = orderService.getOrder(id);
+
+        OptionalOrderResponse orderResponse = new OptionalOrderResponse(200, order, "Order confirmed.");
+
+        return new ResponseEntity<OptionalOrderResponse> (orderResponse, null, 200);
+    }
+
     @PostMapping("/order")
     public ResponseEntity<OrderResponse> newOrder(@RequestBody NewOrder order){
         System.out.println(order);
 
         if (order.creditCard.length() <= 13 || order.creditCard.length() > 19 ){
-            OrderResponse response  = new OrderResponse(400, "Invalid credit card.");
+            OrderResponse response  = new OrderResponse(400, "N/A", "Invalid credit card.");
              return new ResponseEntity<OrderResponse> (response, null, 400);
             
         } else if (order.exp.length() != 4 ){
-            OrderResponse response  = new OrderResponse(400, "Invalid Expiry Date. Please provide expiry Date in MMYY format.");
+            OrderResponse response  = new OrderResponse(400, "N/A", "Invalid Expiry Date. Please provide expiry Date in MMYY format.");
              return new ResponseEntity<OrderResponse> (response, null, 400);
             
         } else if (order.svc.length() != 3){
-            OrderResponse response  = new OrderResponse(400, "Invalid SVC. ");
+            OrderResponse response  = new OrderResponse(400, "N/A", "Invalid SVC. ");
              return new ResponseEntity<OrderResponse> (response, null, 400);
             
         } else {
@@ -41,11 +48,11 @@ public class OrderController {
             Order orderCreated = orderService.addOrder(newOrder);
 
             if(orderCreated != null){
-                OrderResponse response = new OrderResponse(200, "Order succesfully created!");
+                OrderResponse response = new OrderResponse(200, orderCreated.get_id(), "Order succesfully created!");
 
                 return new ResponseEntity<OrderResponse> (response, null, 200);
             } else {
-                OrderResponse response = new OrderResponse(500, "Error encountered while processing order!");
+                OrderResponse response = new OrderResponse(500, "N/A", "Error encountered while processing order!");
 
                 return new ResponseEntity<OrderResponse> (response, null, 500);
             }
@@ -69,6 +76,13 @@ public class OrderController {
 
     record OrderResponse(
         int status,
+        String data,
+        String message
+    ){};
+
+        record OptionalOrderResponse(
+        int status,
+        Optional<Order> data,
         String message
     ){};
 }
--- src/main/java/com/gogo_ecommerce/gogo/order/OrderService.java
+++ src/main/java/com/gogo_ecommerce/gogo/order/OrderService.java
@@ -1,5 +1,7 @@
 package com.gogo_ecommerce.gogo.order;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,10 @@ import org.springframework.stereotype.Service;
 public class OrderService {
     @Autowired
     OrderRepository orderRepository;
+
+    public Optional<Order> getOrder(String id){
+        return orderRepository.findById(id);
+    }
 
     public Order addOrder(Order order){
         return orderRepository.insert(order);
